a182e4aba0a246204e0e645f7b27c2d5a8375fa1
Robert Si <robertsi0214@gmail.com>
2017-12-03 14:28:39 -0800

    add logic to render random colors on page refresh

--- colorGame.js
+++ colorGame.js
@@ -1,11 +1,4 @@
-var colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)"
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
@@ -46,4 +39,28 @@ function pickColor() {
   console.log(colors.length + 1)
   var random = Math.floor(Math.random() * colors.length);
   return colors[random];
+}
+
+function generateRandomColors(num) {
+  // goal: return an array of random colors
+  //make array
+  var randomColorsArray = [];
+  //add num random colors to array
+    for (var i = 0; i < num; i++) {
+      randomColorsArray.push(randomColor())
+    }
+  //return array;
+  return randomColorsArray;
+
+}
+
+function randomColor() {
+  //pick red from 0 - 255
+  var r = Math.floor(Math.random() * 256);
+  //pick green from 0 - 255
+  var g = Math.floor(Math.random() * 256);
+  //pick blue from 0 - 255
+  var b = Math.floor(Math.random() * 256);
+  //synthesize into big string "rgb(r, g, b)"
+  return `rgb(${r}, ${g}, ${b})`;
 }
