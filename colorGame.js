33f18beb130a0d52bf5ccbd07d30b6bf3275b45c
Robert Si <robertsi0214@gmail.com>
2017-12-03 14:07:53 -0800

    add functionality to change color to correct color when user guesses correctly

--- colorGame.js
+++ colorGame.js
@@ -29,6 +29,16 @@ for (var i = 0; i < squares.length; i++) {
     }
     else {
       messageDisplay.textContent = "Correct";
+      changeColors(clickedColor);
     }
   });
+}
+
+function changeColors(color) {
+  //loop through all squares
+  for (var i = 0; i < squares.length; i++) {
+    //change color to match given color
+    squares[i].style.backgroundColor = color;
+  }
+
 }
